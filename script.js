d52fc1ac70770aed63e6b7c832d6b82375addaa8
mjhwest89 <mjhwest89@gmail.com>
2021-09-18 12:52:38 +0930

    addiontal changes to test

--- script.js
+++ script.js
@@ -70,6 +70,13 @@ function generatePassword() {
 
     return "finalRandomPass";
 
+    function writePassword() {
+        var password = generatePassword();
+        var passwordText = document.querySelector("#password");
+
+        passwordText.value = password;
+    }
+
 }
 
 // Add event listener to generate button
