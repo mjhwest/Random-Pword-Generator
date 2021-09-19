11d46ff7f567a9c30b120a2f61fc77df8e1f5269
mjhwest89 <mjhwest89@gmail.com>
2021-09-19 15:07:58 +0930

    deleted bulk as it was confusing me, conducted lots of research and fixed up errors, created a user validation input and set parameters

--- script.js
+++ script.js
@@ -1,89 +1,93 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+
+// setting the password characters object 
+var characterOptions = {
+    numbers: "0123456789",
+    specialChar: "!@#$%^&*()?><}{~",
+    lowerLet: "abcdefghijklmnopqrstuvwxyz",
+    upperLet: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+}
+
+
 // Write password to the #password input 
 function writePassword() {
     var password = generatePassword();
-    var passwordText = document.querySelector("password");
+    var passwordText = document.querySelector("#password");
 
     passwordText.value = password;
 }
 
 // Function to generate a password 
+function generatePassword() {
 
 
-// var generatorLength = prompt("How many characters do you want your password to be, it must be between 8-128."); - from // to - undo
+    // allowing user to select random password length 
+    var generatorLength = prompt("How many characters do you want your password to be, it must be between 8-128.");
 
-// Validate User Input ----NOT WORKING -green out from 18to 31
-// function selectPasswordLength(userCharacters) {
-//     if (isNaN(userCharacters)) {
-//         alert("Enter a valid number.");
-//         return false;
-//     } else if (parseInt(userCharacters) < 8) {
-//         alert("Password is too short, must be at least 8 characters long.");
-//         return false;
-//     } else if (parseInt(userCharacters) > 128) {
-//         alert("Password is too long, must be less than 128 characters long.");
-//         return false;
-
-//     } else
-//         return true;
-// }
-
-// Inputing array options that can be randomly assigned to password. 
-var characterOptions = {
-    numbers: "0123456789",
-    specialChar: "\"\\ !#$%&'()*+,-./:;<=>?@[\]^_`{|}~",
-    lowerLet: "abcdefghijklmnopqrstuvwxyz",
-    upperLet: "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-}
-
-var generatePassword = function() {
-    // Ask user if they want to make a password 
-    var userChoice = window.prompt("How many characters do you want your password to be, it must be between 8-128.");
-    // if they dont want to make a password, press
+    // validating user input selection..."!"
     if (!generatorLength) {
-        return
+        alert('you didnt pick a number :(');
+    } else if (generatorLength < 8) {
+        alert('not enough numbers!');
+    } else if (isNaN(generatorLength)) {
+        alert("Needs to be a number") //isNaN is checking the vaule required is legal, i.e. is a number
+    } else if (generatorLength > 128) {
+        alert('too many numbers!');
+    } else {
+        alert('just right')
+
+
+        // Setting the parameters and confirming if the user wants to include each parameter in the password 
+        paraNumbers = confirm("Select ok to confirm numbers will be incuded or cancel if you dont want numbers included");
+        paraSpecialChar = confirm("Select ok to confirm special characters will be included or cancel if you dont want special characters included");
+        paraLowerLet = confirm("Select ok to confirm lower case letters will be included or cancel if you dont want lower case letters included");
+        paraUpperLet = confirm("Select ok to confirm upper case letters will be included or cancel if you dont want upper case letter to be included ");
+
+        var password = "" //creating an empty string for the random password to go into
+
+        var random = [] //creating an array that will have random character options pushed to it 
+
+        if (paraNumbers) {
+            var num = characterOptions.numbers.split("") //if statement for numbers parameter i.e. paraNumber that if user wants to have 
+            Array.prototype.push.apply(random, num) //if numbers are selected they are pushed to array variable 'random' 
+        }
+
+        if (paraSpecialChar) {
+            var special = characterOptions.specialChar.split("") //same as for numbers 
+            Array.prototype.push.apply(random, special)
+        }
+
+        if (paraLowerLet) {
+            var lower = characterOptions.lowerLet.split("") //same as for numbers 
+            Array.prototype.push.apply(random, lower)
+        }
+
+        if (paraUpperLet) {
+            var upper = characterOptions.upperLet.split("") //same as for numbers 
+            Array.prototype.push.apply(random, upper)
+        }
+
+        //creating a forloop, variable i=0 is making baseline for variables =, then i < generatorLength is the prompt condition 
+        //that user selects i.e. how long the password will be, the i++ is an incrmeent increading the generaotr length by 1 
+        //every time the loop condition is true, until it is reached the end of condition 
+
+        //making array results random using 'array.protype.push.apply' to variable random
+        for (var i = 0; i <= generatorLength; i++) {
+            var prePassWord = Math.floor(Math.random() * random.length)
+            password += random[prePassWord]
+
+        }
+        //ending the function and creating random password, that user selectes character legnth and if they want nuumbers, special characters, upper and lower cases letters to be included in it. 
+        return password;
+
     }
 
-    // Get random index from array options 
-    var index = Math.floor(Math.random() * parameters.length)
-    var parameters = characterOptions("")
-
-
-
-
-    // Setting the password parameters, i.e giving prompts for user to decide what is included in password
-    var parameters = ""
-    paraNumbers = confirm("Select ok to confirm numbers will be incuded or cancel if you dont want numbers included");
-    paraSpecialChar = confirm("Select ok to confirm special characters will be included or cancell if you dontwant special characters included");
-    paraLowerLet = confirm("Select ok to confirm lower case letters will be included or cancel if you dont want lower case letters included");
-    paraUpperLet = confirm("Select ok to confirm upper case letters will be included or cancel if you dont want upper case letter to be included ");
-
-
-    // setting the condition of the array. 
-    if (paraNumbers) {
-        parameters += characterOptions.numbers
-    }
-
-    if (paraSpecialChar) {
-        parameters += characterOptions.specialChar
-    }
-
-    if (paraLowerLet) {
-        parameters += characterOptions.lowerLet
-    }
-
-    if (paraUpperLet) {
-        parameters += characterOptions.upperLet
-    }
-
-    var finalRandomPass = ""
-    for (i = 1; i <= generatorLength; i++) {
-        finalRandomPass += parameters.charAt(Math.floor(Math.random() * parameters.length))
-    }
 
 }
+
+
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
